c14136fa6ddb105e459d975216073e02916412a6
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-11-14 23:59:49 +0500

    Task3

---
+++ Tyuiu.KrasyukME.Sprint4.Task3.V5/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KrasyukME.Sprint4.Task3.V5.Lib;
+namespace Tyuiu.KrasyukME.Sprint4.Task3.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Красюк М. Е. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            int[,] Array = new int[5, 5] { { 3, 4, 8, 6, 3 }, { 9, 9, 3, 6, 9 },
+                { 5, 6, 7, 2, 3 }, { 3, 6, 2, 3, 8 }, { 5, 7, 2, 6, 4 } };
+            int rows = Array.GetUpperBound(0) + 1;
+            int columns = Array.Length / rows;
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{Array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма элементов первого столбца " + ds.Calculate(Array));
+            Console.ReadKey();
+        }
+    }
+}

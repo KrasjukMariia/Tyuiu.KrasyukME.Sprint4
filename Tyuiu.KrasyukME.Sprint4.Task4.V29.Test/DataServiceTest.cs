0e4d7e33bca0327de468337cb84c66d7e6c77052
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-11-15 00:16:26 +0500

    Task4

---
+++ Tyuiu.KrasyukME.Sprint4.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.KrasyukME.Sprint4.Task4.V29.Lib;
+namespace Tyuiu.KrasyukME.Sprint4.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas4 = new int[5, 5] { { 5, 6, 5, 6, 5 },
+                                          { 5, 5, 5, 8, 4 },
+                                          { 8, 5, 5, 8, 8 },
+                                          { 6, 5, 4, 4, 8 },
+                                          { 8, 5, 6, 8, 8 } };
+
+            int res = ds.Calculate(mas4);
+            int wait = 100;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KrasyukME.Sprint4.Task4.V29/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.KrasyukME.Sprint4.Task4.V29.Lib;
+namespace Tyuiu.KrasyukME.Sprint4.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Красюк М. Е. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            Console.WriteLine("Введите количество строк: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Сумма четных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}

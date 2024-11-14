daaf005e8a32f6074a6c997ad21277a9a15aa68f
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-11-14 23:33:39 +0500

    Task1

---
+++ Tyuiu.KrasyukME.Sprint4.Task1.V4/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.KrasyukME.Sprint4.Task1.V4.Lib;
+namespace Tyuiu.KrasyukME.Sprint4.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #4 | Выполнил: Красюк М. Е. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int len;
+            Console.WriteLine("Введите количество элменетов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] numsArray = new int[len];
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элемента массива");
+                numsArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine(numsArray[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(numsArray);
+            Console.WriteLine(res);
+        }
+    }
+}
